1dc8e865719e660c4f4f5562cab78f788443020d
Edwin <>
2023-12-08 12:51:37 -0800

    feat: update

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -7,7 +7,14 @@ const MessageBox = () => {
   const { messages, receiver } = useContext(ChatContext);
 
   return (
-    <Box sx={{ flexGrow: 1, padding: 3 }}>
+    <Box
+      sx={{
+        flexGrow: 1,
+        padding: 3,
+        overflow: "auto",
+        maxHeight: "calc(100vh - 230px)",
+      }}
+    >
       {receiver &&
         messages?.map((message: Message, index) => (
           <MessageItem message={message} key={index} />
